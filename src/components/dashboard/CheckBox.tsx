8ef10c92d540aaf2b17cdadf37cd8d87ec9da5d7
Your Name <you@example.com>
2025-05-16 17:23:38 +0400

    feat: create CheckBox component

---
+++ src/components/dashboard/CheckBox.tsx
@@ -0,0 +1,16 @@
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
+
+interface CheckBoxProps {
+  defaultChecked?: boolean;
+  label: string;
+}
+
+export default function CheckBox({ defaultChecked = false, label }: CheckBoxProps) {
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox defaultChecked={defaultChecked} />
+      <Label htmlFor="terms">{label}</Label>
+    </div>
+  )
+}
