fa2deb524d855ec94d842370158898d92b4f86f6
Your Name <you@example.com>
2025-07-18 17:12:23 +0400

    feat: add request wrapper

---
+++ src/providers/request.ts
@@ -0,0 +1,19 @@
+import { storage, tokenStorageKey } from './AuthProvider'
+
+async function _get(url: string) {
+    // TODO: Implement.
+}
+
+async function _delete(url: string) {
+    // TODO: Implement.
+}
+
+async function _post(url: string) {
+    // TODO: Implement.
+}
+
+export default {
+    delete: _delete,
+    get: _get,
+    post: _post,
+}
