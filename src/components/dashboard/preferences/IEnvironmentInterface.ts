8c1851b9db6af448629d56bbe14fa24b89b5bafe
Your Name <you@example.com>
2025-07-23 13:48:41 +0400

    feat: add IEnvironmentInterface

---
+++ src/components/dashboard/preferences/IEnvironmentInterface.ts
@@ -0,0 +1,10 @@
+interface IEnvironmentInterface {
+    action?: string
+    user?: boolean
+    name?: string
+    partial?: boolean
+    value?: string
+    system?: boolean
+}
+
+export type { IEnvironmentInterface }
