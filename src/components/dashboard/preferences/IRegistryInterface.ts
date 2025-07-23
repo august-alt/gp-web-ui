0ce6112dc3922132b12e285cec01af2339056b32
Your Name <you@example.com>
2025-07-23 13:32:54 +0400

    feat: add IRegistryInterface

---
+++ src/components/dashboard/preferences/IRegistryInterface.ts
@@ -0,0 +1,11 @@
+interface IRegistryInterface {
+    action?: string
+    hive?: string
+    key?: string
+    name?: string
+    type?: string
+    value?: string
+    default?: string
+}
+
+export type { IRegistryInterface }
