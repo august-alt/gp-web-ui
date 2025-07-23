ac1d6ad9275281ed5ee71061b9f534ff59c9ed7c
Your Name <you@example.com>
2025-07-23 14:24:22 +0400

    feat: add IShortcutInterface

---
+++ src/components/dashboard/preferences/IShortcutInterface.ts
@@ -0,0 +1,17 @@
+interface IShortcutInterface {
+    action?:string
+    pidl?:string
+    shortcutPath?:string
+    targetType?:number
+    targetPath?:string
+    location?:number
+    arguments?:string
+    startIn?:string
+    window?:number
+    comment?:string
+    iconPath?:string
+    iconIndex?:string
+    shortcutKey?:string
+}
+
+export type { IShortcutInterface }
