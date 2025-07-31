1a10d77d88898f2bc4080398db1da6e9ce213ffa
Your Name <you@example.com>
2025-07-31 13:17:26 +0400

    feat: add explain text to VariablesWidget

--- src/components/dashboard/preferences/VariablesWidget.tsx
+++ src/components/dashboard/preferences/VariablesWidget.tsx
@@ -30,6 +30,102 @@ export function VariablesWidget({sourceItem, updateData}:ShortcutsWidgetProps) {
     });
   };
 
+  const getRadioState = () => {
+    if (varData?.user)
+     return "user";
+    if (varData?.system)
+     return "system";
+    return "user";
+  }
+
+  const getExplainText = (action: number, partial: boolean, path: boolean) => {
+    switch (action)
+    {
+      case 1:
+        if (path)
+          {
+            if (partial)
+            {
+              return "The PATH variable contains a list of folder paths that system uses when" +
+                     " locating executable files. Replace will replace" +
+                     " the specified part of the path variable. This will have no net" +
+                     " effect on the path with the possible exception of changing the" +
+                     " text case of the specified segment. Multiple segments are not" +
+                     " supported by this option.";
+            }
+            else
+            {
+              return "The PATH variable contains a list of folder paths that system uses when" +
+                     " locating executable files. Replace will replace the FULL path" +
+                     " variable as specified. This will remove all previously existing" +
+                     " path values.";
+            }
+          }
+          return "Replace will replace the variable in the specified environment." +
+                 " If it doesn't exist, it will be created.";
+      case 2:
+        if (path)
+          {
+            if (partial)
+            {
+              return "The PATH variable contains a list of folder paths that system uses when" +
+                     " locating executable files. Update will replace the specified" +
+                     " part of the path variable. This will have no net effect on the path with" +
+                     " the possible exception of changing the text case of the specified" +
+                     " segment. Multiple segments are not supported by this option.";
+            }
+            else
+            {
+              return "The PATH variable contains a list of folder paths that system uses when" +
+                     " locating executable files. Update will replace the FULL" +
+                     " path variable as specified. This will remove all previously existing" +
+                     " path values.";
+            }
+          }
+          return "Update will replace the variable in the specified environment." +
+                 " If it doesn't exist, it will be created.";
+      case 3:
+        if (path)
+          {
+            if (partial)
+            {
+              return "The PATH variable contains a list of folder paths that system uses when" +
+                     " locating executable files. Delete will remove the specified" +
+                     " segment from the existing path variable. If the segment doesn't exist," +
+                     " an error is not returned. Multiple segments are not supported" +
+                     " by this option.";
+            }
+            else
+            {
+              return "The PATH variable contains a list of folder paths that system uses when" +
+                     " locating executable files. Delete will delete the FULL path" +
+                     " variable. If it doesn't exist, an error is not returned. This" +
+                     " setting is not generally recommended.";
+            }
+          }
+          return "Delete will remove the variable from the specified environment." +
+                 " If it doesn't exist, an error is not returned.";
+    }
+
+    if (path)
+    {
+      if (partial)
+      {
+        return "Create will add the variable to the specified environment." +
+               " If it already exists, the value will not be changed.";
+      }
+      else
+      {
+        return "The PATH variable contains a list of folder paths that system" +
+               "uses when locating executable files." +
+               " Create will create the full path variable as specified if" +
+               " it does not exist. Generally the path will already exist.";
+      }
+    }
+    return "Create will add the variable to the specified environment." +
+           " If it already exists, the value will not be changed.";
+  }
+
   return (
     <div className="space-y-4">
       {/* Action ComboBox Section */}
@@ -60,7 +156,7 @@ export function VariablesWidget({sourceItem, updateData}:ShortcutsWidgetProps) {
             defaultValue="user"
             className="flex flex-col space-y-1"
             name="system"
-            value={ varData?.user ? "user" : varData?.system ? "system" : "user" }
+            value={getRadioState()}
             onValueChange={
               (value: string) => {
                 const boolValue = value === "user";
@@ -147,7 +243,7 @@ export function VariablesWidget({sourceItem, updateData}:ShortcutsWidgetProps) {
         </CardHeader>
         <CardContent>
           <p className="text-sm text-muted-foreground">
-            Placeholder text for variable details. This text will wrap automatically to provide additional information about the selected variable.
+            {getExplainText(varData?.action || 0, varData?.partial || false, varData?.name?.toLowerCase() === "path")}
           </p>
         </CardContent>
       </Card>
